f5d634b23ad54e9734d4e91ce728db4281e16e65
Sumedh Kamble <sumedhkamble0@gmail.com>
2021-12-05 16:02:25 +0530

    hard commiting.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function(){
 
   $('#menu').click(function(){
@@ -37,46 +36,3 @@ $(document).ready(function(){
   });
 
 });
-
-
-
-=======
-$(document).ready(function(){
-
-  $('#menu').click(function(){
-    $(this).toggleClass('fa-times');
-    $('header').toggleClass('toggle');
-  });
-
-  $(window).on('scroll load',function(){
-
-    $('#menu').removeClass('fa-times');
-    $('header').removeClass('toggle');
-
-    if($(window).scrollTop() > 0){
-      $('.top').show();
-    }else{
-      $('.top').hide();
-    }
-
-  });
-
-  // smooth scrolling 
-
-  $('a[href*="#"]').on('click',function(e){
-
-    e.preventDefault();
-
-    $('html, body').animate({
-
-      scrollTop : $($(this).attr('href')).offset().top,
-
-    },
-      500, 
-      'linear'
-    );
-
-  });
-
-});
->>>>>>> af5a28d0f7cb5adf2327e28ec0ac0c354eda4fc6
